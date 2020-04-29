40c16a9ed9acc7ffa2ad1e24694563530e563806
Daniel <drebolledo@acid.cl>
2020-04-29 11:39:04 -0400

    token access

--- src/Contentful.js
+++ src/Contentful.js
@@ -3,5 +3,5 @@ import { createClient } from 'contentful'
 export default createClient({
   host: process.env.REACT_APP_CONTENTFUL_HOST,
   space: process.env.REACT_APP_API_SPACE,
-  accessToken: 'rbXjT3dQl2smfGM4fJdQYJ4VnA0Ypgzkpft7GsrvYqU',
+  accessToken: process.env.REACT_APP_ACCESS_TOKEN,
 });
