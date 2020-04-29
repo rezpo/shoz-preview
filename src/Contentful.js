8d3321ee43316cb903d082312ace95d60af581af
Daniel <drebolledo@acid.cl>
2020-04-28 21:22:06 -0400

    preview enviroment config

--- src/Contentful.js
+++ src/Contentful.js
@@ -1,6 +1,7 @@
 import { createClient } from 'contentful'
 
 export default createClient({
+  host: process.env.REACT_APP_CONTENTFUL_HOST,
   space: process.env.REACT_APP_API_SPACE,
   accessToken: process.env.REACT_APP_ACCESS_TOKEN,
 });
